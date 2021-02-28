fe5c510825d3bbbf62392ba85b4fc1e7ef673ace
sanimridha <sanimridha307@gmail.com>
2021-02-28 12:54:02 +0600

    init Onboarding Screen

---
+++ screen/OnboardingScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+
+const OnboardingScreen = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <Text>Onboarding Screen</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+        <Text>Click Here</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default OnboardingScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

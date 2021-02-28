081e62542f7d403085822602a4d405dc598af2ff
sanimridha <sanimridha307@gmail.com>
2021-02-28 13:42:17 +0600

    headerMode none

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ const AppStack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <AppStack.Navigator>
+      <AppStack.Navigator headerMode="none">
         <AppStack.Screen name="Onboarding" component={OnboardingScreen} />
         <AppStack.Screen name="Login" component={LoginScreen} />
       </AppStack.Navigator>

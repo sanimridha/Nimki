c9c8e2dc49952e37dd2a475eac7f3c12c9f65d9f
sanimridha <sanimridha307@gmail.com>
2021-03-01 12:48:04 +0600

    added signup screen

---
+++ screen/SignupScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const SignupScreen = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export default SignupScreen;
+
+const styles = StyleSheet.create({});

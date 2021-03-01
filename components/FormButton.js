844a4ed3e954933220305f2c156757f8f1cfe01b
sanimridha <sanimridha307@gmail.com>
2021-03-01 12:47:17 +0600

    component for default button

---
+++ components/FormButton.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import Dimentions, {WindowHeight} from '../utils/Dimentions';
+
+const Formbutton = ({buttonTitle, ...rest}) => {
+  return (
+    <TouchableOpacity style={styles.buttonContainer} {...rest}>
+      <Text style={styles.buttonText}>{buttonTitle}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    marginTop: 10,
+    width: '100%',
+    height: WindowHeight / 15,
+    backgroundColor: '#2e64e5',
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 3,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#ffffff',
+    fontFamily: 'Lato-Regular',
+  },
+});
+
+export default Formbutton;

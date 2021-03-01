289b63914c11bcdf8219cf1f1eea71b31165fcde
sanimridha <sanimridha307@gmail.com>
2021-03-01 16:04:41 +0600

    added social button component

---
+++ components/SocialButton.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {WindowHeight, WindowWidth} from '../utils/Dimentions';
+import FontAwesome from "react-native-vector-icons/FontAwesome"
+
+const SocialButton = ({buttonTitle, btnType, color, backgroundColor ...rest}) => {
+    let bgColor= backgroundColor;
+  return (
+    <TouchableOpacity style={[styles.buttonContainer, {backgroundColor:bgColor}]} {...rest}>
+        <View style={styles.iconWrapper}>
+            <FontAwesome name={btnType} size={22} color={color} style={styles.icon}/>
+        </View>
+        <View>
+      <Text style={[styles.buttonText, {color:color}]}>{buttonTitle}</Text>
+        </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    marginTop: 10,
+    width: '100%',
+    height: WindowHeight / 15,
+    padding: 10,
+    flexDirection:"row",
+    borderRadius: 3,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    fontFamily: 'Lato-Regular',
+  },
+  iconWrapper:{
+    width:30,
+    justifyContent:"center",
+    alignItems:"center",
+  },
+  icon:{
+      fontWeight:"bold"
+  },
+  btnTxtWrapper:{
+      flex:1,
+      justifyContent:"center",
+      alignItems:"center"
+  }
+});
+
+export default SocialButton;

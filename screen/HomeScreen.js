efa44fb7da91977b89fd5168907115490034b448
sanimridha <sanimridha307@gmail.com>
2021-03-03 13:34:41 +0600

    added button for testing

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import Formbutton from '../components/FormButton';
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Home</Text>
+      <Formbutton buttonTitle="Logout" onPress={() => {}} />
     </View>
   );
 };
 
-export default HomeScreen;
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f9fafd',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  text: {
+    fontSize: 20,
+    color: '#333333',
+  },
+});
 
-const styles = StyleSheet.create({});
+export default HomeScreen;

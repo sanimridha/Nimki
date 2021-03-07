bfda9ddbe0a142ac2652839b56cc04449a2d3707
sanimridha <sanimridha307@gmail.com>
2021-03-07 14:47:41 +0600

    plartform specified packages

--- react-native.config.js
+++ react-native.config.js
@@ -1,3 +1,20 @@
 module.exports = {
+  projects: {
+    ios: {},
+    android: {},
+  },
+  dependencies: {
+    '@react-native-google-signin/google-signin': {
+      plartforms: {
+        ios: null,
+      },
+    },
+    'react-native-fbsdk': {
+      plartforms: {
+        ios: null,
+      },
+    },
+  },
+
   assets: ['./assets/fonts'],
 };

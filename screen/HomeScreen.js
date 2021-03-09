57ff32dcd57d89a44d8a284bd2a65af4395f0749
sanimridha <sanimridha307@gmail.com>
2021-03-09 12:56:42 +0600

    added dummy data into homescreen

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -17,53 +17,74 @@ import {
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {StyleSheet, View} from 'react-native';
 
+const Posts = [
+  {
+    id: '1',
+    userName: 'Jenny Doe',
+    userImg: require('../assets/users/user-3.jpg'),
+    postTime: '4 mins ago',
+    post:
+      'Hey there, this is my test for a post of my social app in React Native.',
+    postImg: require('../assets/posts/post-img-3.jpg'),
+    liked: true,
+    likes: '14',
+    comments: '5',
+  },
+  {
+    id: '2',
+    userName: 'John Doe',
+    userImg: require('../assets/users/user-1.jpg'),
+    postTime: '2 hours ago',
+    post:
+      'Hey there, this is my test for a post of my social app in React Native.',
+    postImg: 'none',
+    liked: false,
+    likes: '8',
+    comments: '0',
+  },
+  {
+    id: '3',
+    userName: 'Ken William',
+    userImg: require('../assets/users/user-4.jpg'),
+    postTime: '1 hours ago',
+    post:
+      'Hey there, this is my test for a post of my social app in React Native.',
+    postImg: require('../assets/posts/post-img-2.jpg'),
+    liked: true,
+    likes: '1',
+    comments: '0',
+  },
+  {
+    id: '4',
+    userName: 'Selina Paul',
+    userImg: require('../assets/users/user-6.jpg'),
+    postTime: '1 day ago',
+    post:
+      'Hey there, this is my test for a post of my social app in React Native.',
+    postImg: require('../assets/posts/post-img-4.jpg'),
+    liked: true,
+    likes: '22',
+    comments: '4',
+  },
+  {
+    id: '5',
+    userName: 'Christy Alex',
+    userImg: require('../assets/users/user-7.jpg'),
+    postTime: '2 days ago',
+    post:
+      'Hey there, this is my test for a post of my social app in React Native.',
+    postImg: 'none',
+    liked: false,
+    likes: '0',
+    comments: '0',
+  },
+];
+
 const HomeScreen = () => {
   return (
     <Container>
       {/* <View style={styles.container}> */}
-      <Card>
-        <UserInfo>
-          <UserImg source={require('../assets/users/user-3.jpg')} />
-          <UserInfoText>
-            <UserName>Alexzendra Daddario</UserName>
-            <PostTime>4 hours ago</PostTime>
-          </UserInfoText>
-        </UserInfo>
-        <PostText>hey there... how are you guys?</PostText>
-        <PostImg source={require('../assets/posts/post-img-1.jpg')} />
-        <InteractionWrapper>
-          <Interaction>
-            <Ionicons name="heart-outline" size={24} color="black" />
-            <InteractionText>Like</InteractionText>
-          </Interaction>
-          <Interaction>
-            <Ionicons name="md-chatbubble-outline" size={24} color="black" />
-            <InteractionText>Comment</InteractionText>
-          </Interaction>
-        </InteractionWrapper>
-      </Card>
-      <Card>
-        <UserInfo>
-          <UserImg source={require('../assets/users/user-3.jpg')} />
-          <UserInfoText>
-            <UserName>Alexzendra Daddario</UserName>
-            <PostTime>4 hours ago</PostTime>
-          </UserInfoText>
-        </UserInfo>
-        <PostText>hey there... how are you guys?</PostText>
-        {/* <PostImg source={require('../assets/posts/post-img-1.jpg')} /> */}
-        <Divider />
-        <InteractionWrapper>
-          <Interaction active>
-            <Ionicons name="heart" size={24} color="#2e64e5" />
-            <InteractionText active>12 Likes</InteractionText>
-          </Interaction>
-          <Interaction>
-            <Ionicons name="md-chatbubble-outline" size={24} color="black" />
-            <InteractionText>Comment</InteractionText>
-          </Interaction>
-        </InteractionWrapper>
-      </Card>
+
       {/* </View> */}
     </Container>
   );

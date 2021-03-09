38268c6e0d657f95566da0646241321476635fbb
sanimridha <sanimridha307@gmail.com>
2021-03-09 15:41:31 +0600

    added post card component

---
+++ components/PostCard.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {
+  Card,
+  Container,
+  UserInfo,
+  UserImg,
+  PostTime,
+  UserName,
+  UserInfoText,
+  PostText,
+  PostImg,
+  InteractionWrapper,
+  Interaction,
+  InteractionText,
+  Divider,
+} from '../styles/FeedStyles';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const PostCard = ({item}) => {
+  var likeIcon = item.liked ? 'heart' : 'heart-outline';
+  var likeIconColor = item.liked ? '#2e64e5' : '#333';
+  var likeText, commentText;
+  if (item.likes == 1) {
+    likeText = '1 Like';
+  } else if (item.likes > 1) {
+    likeText = item.likes + ' Likes';
+  } else {
+    likeText = 'Like';
+  }
+
+  if (item.comments == 1) {
+    commentText = '1 Comment';
+  } else if (item.comments > 1) {
+    commentText = item.comments + ' Comments';
+  } else {
+    commentText = 'Comment';
+  }
+  return (
+    <Card>
+      <UserInfo>
+        <UserImg source={item.userImg} />
+        <UserInfoText>
+          <UserName>{item.userName}</UserName>
+          <PostTime>{item.postTime}</PostTime>
+        </UserInfoText>
+      </UserInfo>
+      <PostText>{item.post}</PostText>
+      {item.postImg != 'none' ? <PostImg source={item.postImg} /> : <Divider />}
+      <InteractionWrapper>
+        <Interaction active={item.liked}>
+          <Ionicons name={likeIcon} size={24} color={likeIconColor} />
+          <InteractionText active={item.liked}>{likeText}</InteractionText>
+        </Interaction>
+        <Interaction>
+          <Ionicons name="md-chatbubble-outline" size={24} color="black" />
+          <InteractionText>{commentText}</InteractionText>
+        </Interaction>
+      </InteractionWrapper>
+    </Card>
+  );
+};
+
+export default PostCard;

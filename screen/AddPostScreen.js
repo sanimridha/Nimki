e6ef3e441c3cd8b08fdd7c06e09fc46ac7f56cbd
sanimridha <sanimridha307@gmail.com>
2021-03-09 23:28:28 +0600

    added added post screen

--- screen/AddPostScreen.js
+++ screen/AddPostScreen.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {InputField, InputWrapper} from '../styles/AddPost';
 
 const AddPostScreen = () => {
   return (
-    <View>
-      <Text>Add Post Screen</Text>
+    <View style={styles.container}>
+      <InputWrapper>
+        <InputField
+          placeholder="What's on your mind?"
+          multiline
+          numberOfLines={4}
+        />
+      </InputWrapper>
     </View>
   );
 };
 
 export default AddPostScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

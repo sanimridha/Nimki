defa407817a3b288fb4cb149895e08bc59f50d31
sanimridha <sanimridha307@gmail.com>
2021-03-10 00:06:21 +0600

    added floating buttons

--- screen/AddPostScreen.js
+++ screen/AddPostScreen.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {FloatingAction} from 'react-native-floating-action';
+import Icon from 'react-native-vector-icons/Ionicons';
+
 import {InputField, InputWrapper} from '../styles/AddPost';
 
 const AddPostScreen = () => {
+  const actions = [
+    {
+      text: 'Take Photo',
+      icon: <Icon name="camera-outline" style={styles.actionButtonIcon} />,
+      name: 'bt_takePhoto',
+      position: 3,
+      buttonSize: 46,
+      color: 'orange',
+    },
+    {
+      text: 'Choose Photo',
+      icon: <Icon name="md-images-outline" style={styles.actionButtonIcon} />,
+      name: 'bt_choosePhoto',
+      position: 2,
+      buttonSize: 46,
+      color: 'green',
+    },
+  ];
   return (
     <View style={styles.container}>
       <InputWrapper>
@@ -12,6 +33,13 @@ const AddPostScreen = () => {
           numberOfLines={4}
         />
       </InputWrapper>
+      <FloatingAction
+        actions={actions}
+        onPressItem={(name) => {
+          console.log(`selected button: ${name}`);
+        }}
+        color="red"
+      />
     </View>
   );
 };
@@ -23,5 +51,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  actionButtonIcon: {
+    fontSize: 20,
+    height: 22,
+    color: 'white',
   },
 });

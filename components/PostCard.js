f8666f731d10541724ae04850b873b5e12782326
sanimridha <sanimridha307@gmail.com>
2021-03-20 13:12:40 +0600

    Fetch Posts or Get Documents from Firestore

--- components/PostCard.js
+++ components/PostCard.js
@@ -38,14 +38,18 @@ const PostCard = ({item}) => {
   return (
     <Card>
       <UserInfo>
-        <UserImg source={item.userImg} />
+        <UserImg source={{uri: item.userImg}} />
         <UserInfoText>
           <UserName>{item.userName}</UserName>
-          <PostTime>{item.postTime}</PostTime>
+          <PostTime>{item.postTime.toString()}</PostTime>
         </UserInfoText>
       </UserInfo>
       <PostText>{item.post}</PostText>
-      {item.postImg != 'none' ? <PostImg source={item.postImg} /> : <Divider />}
+      {item.postImg != null ? (
+        <PostImg source={{uri: item.postImg}} />
+      ) : (
+        <Divider />
+      )}
       <InteractionWrapper>
         <Interaction active={item.liked}>
           <Ionicons name={likeIcon} size={24} color={likeIconColor} />
--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -1,78 +1,129 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Container} from '../styles/FeedStyles';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {FlatList, StyleSheet, View} from 'react-native';
 import PostCard from '../components/PostCard';
+import firestore from '@react-native-firebase/firestore';
 
-const Posts = [
-  {
-    id: '1',
-    userName: 'Jenny Doe',
-    userImg: require('../assets/users/user-3.jpg'),
-    postTime: '4 mins ago',
-    post:
-      'Hey there, this is my test for a post of my social app in React Native.',
-    postImg: require('../assets/posts/post-img-3.jpg'),
-    liked: true,
-    likes: '14',
-    comments: '5',
-  },
-  {
-    id: '2',
-    userName: 'John Doe',
-    userImg: require('../assets/users/user-1.jpg'),
-    postTime: '2 hours ago',
-    post:
-      'Hey there, this is my test for a post of my social app in React Native.',
-    postImg: 'none',
-    liked: false,
-    likes: '8',
-    comments: '0',
-  },
-  {
-    id: '3',
-    userName: 'Ken William',
-    userImg: require('../assets/users/user-4.jpg'),
-    postTime: '1 hours ago',
-    post:
-      'Hey there, this is my test for a post of my social app in React Native.',
-    postImg: require('../assets/posts/post-img-2.jpg'),
-    liked: true,
-    likes: '1',
-    comments: '0',
-  },
-  {
-    id: '4',
-    userName: 'Selina Paul',
-    userImg: require('../assets/users/user-6.jpg'),
-    postTime: '1 day ago',
-    post:
-      'Hey there, this is my test for a post of my social app in React Native.',
-    postImg: require('../assets/posts/post-img-4.jpg'),
-    liked: true,
-    likes: '22',
-    comments: '4',
-  },
-  {
-    id: '5',
-    userName: 'Christy Alex',
-    userImg: require('../assets/users/user-7.jpg'),
-    postTime: '2 days ago',
-    post:
-      'Hey there, this is my test for a post of my social app in React Native.',
-    postImg: 'none',
-    liked: false,
-    likes: '0',
-    comments: '0',
-  },
-];
+// const Posts = [
+//   {
+//     id: '1',
+//     userName: 'Jenny Doe',
+//     userImg: require('../assets/users/user-3.jpg'),
+//     postTime: '4 mins ago',
+//     post:
+//       'Hey there, this is my test for a post of my social app in React Native.',
+//     postImg: require('../assets/posts/post-img-3.jpg'),
+//     liked: true,
+//     likes: '14',
+//     comments: '5',
+//   },
+//   {
+//     id: '2',
+//     userName: 'John Doe',
+//     userImg: require('../assets/users/user-1.jpg'),
+//     postTime: '2 hours ago',
+//     post:
+//       'Hey there, this is my test for a post of my social app in React Native.',
+//     postImg: 'none',
+//     liked: false,
+//     likes: '8',
+//     comments: '0',
+//   },
+//   {
+//     id: '3',
+//     userName: 'Ken William',
+//     userImg: require('../assets/users/user-4.jpg'),
+//     postTime: '1 hours ago',
+//     post:
+//       'Hey there, this is my test for a post of my social app in React Native.',
+//     postImg: require('../assets/posts/post-img-2.jpg'),
+//     liked: true,
+//     likes: '1',
+//     comments: '0',
+//   },
+//   {
+//     id: '4',
+//     userName: 'Selina Paul',
+//     userImg: require('../assets/users/user-6.jpg'),
+//     postTime: '1 day ago',
+//     post:
+//       'Hey there, this is my test for a post of my social app in React Native.',
+//     postImg: require('../assets/posts/post-img-4.jpg'),
+//     liked: true,
+//     likes: '22',
+//     comments: '4',
+//   },
+//   {
+//     id: '5',
+//     userName: 'Christy Alex',
+//     userImg: require('../assets/users/user-7.jpg'),
+//     postTime: '2 days ago',
+//     post:
+//       'Hey there, this is my test for a post of my social app in React Native.',
+//     postImg: 'none',
+//     liked: false,
+//     likes: '0',
+//     comments: '0',
+//   },
+// ];
 
 const HomeScreen = () => {
+  const [posts, setPosts] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const list = [];
+
+        await firestore()
+          .collection('posts')
+          .get()
+          .then((querySnapshot) => {
+            console.log('Total Post: ', querySnapshot.size);
+
+            querySnapshot.forEach((doc) => {
+              const {
+                userId,
+                post,
+                postImg,
+                postTime,
+                likes,
+                comments,
+              } = doc.data();
+              list.push({
+                id: doc.id,
+                userId,
+                userName: 'Test Name',
+                userImg:
+                  'https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Chrome__logo.max-500x500.png',
+                postTime: postTime,
+                post,
+                postImg,
+                liked: false,
+                likes,
+                comments,
+              });
+            });
+          });
+        setPosts(list);
+
+        if (loading) {
+          setLoading(false);
+        }
+        console.log('post : ', posts);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchPost();
+  }, []);
+
   return (
     <Container>
       {/* <View style={styles.container}> */}
       <FlatList
-        data={Posts}
+        data={posts}
         renderItem={({item}) => <PostCard item={item} />}
         keyExtractor={(item) => item.id}
         showsVerticalScrollIndicator={false}

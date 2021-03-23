0f3b8c4b5b2ab43a4178813956af76b8653bfb81
sanimridha <sanimridha307@gmail.com>
2021-03-23 11:23:16 +0600

    init Messages Screen

---
+++ screen/MessagesScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const MessagesScreen = () => {
+  return (
+    <View>
+      <Text>Hey there</Text>
+    </View>
+  );
+};
+
+export default MessagesScreen;
+
+const styles = StyleSheet.create({});
